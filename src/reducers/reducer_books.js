eee18bab4fe6cc47a8a132e45217d0dd6549639b
alonko <kodneralon@gmail.com>
2018-10-07 19:40:21 +1300

    My first Redux commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: "Javascript", pages: 101 },
+        { title: "Harry Poter", pages: 84 },
+        { title: "The dark tower", pages: 204 },
+        { title: "Ruby", pages: 10 }
+    ]
+}
